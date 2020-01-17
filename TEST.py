adb05ca54da434e4a040ead9e671f7a4f007caa5
Borais <lyonya.kizhner@yandex.ru>
2020-01-17 09:24:23 +0300

    Delete TEST.py

--- TEST.py
+++
@@ -1,8 +0,0 @@
-key = 'lol'
-text = 'amigo'
-dictionaryO = {}
-dictionaryO.update({"lol":"amigo"})
-print(dictionaryO)
-dictionaryO.pop("lol")
-dictionaryO = dictionaryO.update({"lol": "parse"})
-print(dictionaryO)
